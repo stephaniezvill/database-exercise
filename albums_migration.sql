01c11b42c07ff9ec8610c135d3e5b6e9022cf166
stephaniezvill <stephanie.zoz.villarreal@gmail.com>
2023-11-27 13:56:12 -0600

    Completed tables exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,9 +4,9 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE `albums` (
                           `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                          `artist` varchar(100) NOT NULL,
-                          `name` varchar(100) NOT NULL,
+                          `artist` varchar(256) NOT NULL,
+                          `name` varchar(255) NOT NULL,
                           `release_date` int(4) NOT NULL,
-                          `sales` float NOT NULL,
-                          `genre` varchar(50) NOT NULL
+                          `sales` decimal(4,1) NOT NULL,
+                          `genre` varchar(1000) NOT NULL
 );
