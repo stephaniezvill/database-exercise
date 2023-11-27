c1c057981a04accb232ef0a7fa47d9bfc22e64eb
stephaniezvill <stephanie.zoz.villarreal@gmail.com>
2023-11-27 12:20:53 -0600

    completed first table titled albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,12 @@
-USE hippo_steph;
+use hippo_steph;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE `albums` (
+                          `id` int unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          `artist` varchar(100) NOT NULL,
+                          `name` varchar(100) NOT NULL,
+                          `release_date` int(4) NOT NULL,
+                          `sales` float NOT NULL,
+                          `genre` varchar(50) NOT NULL
+);
