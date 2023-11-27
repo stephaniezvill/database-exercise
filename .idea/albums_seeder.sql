0bac554673f5c352d5e3e9edbe3c5ba899ea0fc8
stephaniezvill <stephanie.zoz.villarreal@gmail.com>
2023-11-27 16:23:06 -0600

    Completed the insert part of the exercise

---
+++ .idea/albums_seeder.sql
@@ -0,0 +1,37 @@
+use hippo_steph;
+
+insert into albums (artist,name,release_date,sales,genre)
+values ('Bob Marley and the Wailers','Legend: The Best of Bob Marley & The Wailers','1984', '30', 'Reggae'),
+       ('Guns N'' Roses', 'Appetite for Destruction', '1987', '30', 'Hard rock'),
+       ('Bruce Springsteen', 'Born in the U.S.A', '1984', '30', 'Heartland rock'),
+       ('ABBA', 'Gold: Greatest Hits', '1992','30', 'Pop, disco'),
+       ('Dire Straits', 'Brothers in Arms', '1985', '30', 'Roots rock, blues rock, soft rock'),
+       ('Madonna', 'The Immaculate Collection', '1990', '30', 'Pop, dance'),
+       ('Santana', 'Supernatural', '1990', '30', 'Latin rock'),
+       ('Pink Floyd', 'The Wall', '1979', '30', 'Progressive rock'),
+       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '30', 'Rock'),
+       ('Micheal Jackson', 'Dangerous', '1991', '30', 'New jack swing, R&B and pop'),
+       ('The Beatles', 'Abbey Road', '1969', '30', 'Rock'),
+       ('Adele', '21', '2011', '31', 'Pop, soul'),
+       ('The Beatles', '1', '2000', '31', 'Rock'),
+       ('Metallica', 'Metallica', '1991', '31', 'Heavy metal'),
+       ('Celine Dion', 'Let''s Talk About Love', '1997', '31', 'Pop,soft rock'),
+       ('Various artist', 'Dirty Dancing', '1987', '32', 'Pop,rock,R&B'),
+       ('Celine Dion', 'Falling into You', '1996', '32', 'Pop,soft rock'),
+       ('Alanis Morissette', 'Jagged Little Pill', '1995', '33', 'Alternative rock'),
+       ('Micheal Jackson', 'Bad', '1987', '35', 'Pop, rhythm and blues, funk and rock'),
+       ('Led Zeppelin', 'Led Zeppelin IV', '1971', '37', 'Hard rock, heavy metal, folk rock'),
+       ('Bee Gees / Various artist', 'Saturday Night Fever', '1977', '40', 'Disco'),
+       ('Fleetwood Mac', 'Rumours', '1977', '40', 'Soft rock'),
+       ('Shania Twain', 'Come On Over', '1997', '40', 'Country, pop'),
+       ('Eagles', 'Hotel California', '1976', '42', 'Soft rock'),
+       ('Meat Loaf', 'Bat Out of Hell', '1977', '43', 'Hard rock, glam rock, progressive rock'),
+       ('Eagles', 'Their Greatest Hits (1971-1975)', '1976', '44', 'Country rock, soft rock, folk rock'),
+       ('Pink FLoyed', 'The Dark Side of the Moon', '1973', '45', 'Progressive rock'),
+       ('Whitney Houston / Various Artist', 'The Bodyguard', '1992', '45', 'R&B, soul, pop, soundtrack'),
+       ('AC/DC', 'Back in Black', '1980', '50', 'Hard rock'),
+       ('Micheal Jackson', 'Thriller', '1982', '70', 'Pop, post-disco, funk, rock');
+
+
+
+
