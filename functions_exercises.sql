4ea4989fdb561c79e535785e4957e8087cae9713
stephaniezvill <stephanie.zoz.villarreal@gmail.com>
2023-11-29 15:54:51 -0600

    completed my exercise on functions

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,7 +15,7 @@ select * from employees where last_name like 'E%' or last_name like '%E';
 select * from employees where last_name like 'E%' and last_name like '%E';
 
 select * from employees where last_name like'%q%' and last_name not like '%q%';
---modify--
+
 select * from employees where first_name in ('Irene','Vidya', 'Maya') order by first_name;
 
 select * from employees where first_name in ('Irene','Vidya', 'Maya') order by first_name, last_name;
@@ -25,3 +25,19 @@ select * from employees where first_name in ('Irene','Vidya', 'Maya') order by l
 select * from employees where last_name like 'E%' and last_name like '%E' order by emp_no desc;
 
 select * from employees where last_name like 'E%' and last_name like '%E' order by emp_no desc;
+
+#UPDATE
+select concat(first_name,' ', last_name) from employees where first_name like '%E%' and last_name like '%E%';
+
+select * from employees where year(birth_date) between 1900 and 1999 and month(birth_date) = 12 and day (birth_date) = 25;
+
+select * from employees where month(birth_date) = 12 and day(birth_date) = 25 and year(hire_date) between 1990 and 1999;
+
+select * from employees where month(birth_date) = 12 and day(birth_date) = 25 and year(hire_date) between 1990 and 1999 order by hire_date desc;
+
+select datediff(hire_date,curdate()) / 365, hire_date from employees where month(birth_date) = 12 and day(birth_date) = 25 and year(hire_date) between 1990 and 1999 order by hire_date desc;
+
+SELECT *, DATEDIFF(NOW(), hire_date) AS days_worked
+FROM employees
+WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25 AND YEAR(hire_date) BETWEEN 1990 AND 1999
+ORDER BY hire_date DESC;
