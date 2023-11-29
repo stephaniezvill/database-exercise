2a1f8173fa3f45d2f3f7918df21040800f380ffe
stephaniezvill <stephanie.zoz.villarreal@gmail.com>
2023-11-29 11:36:56 -0600

    completed order by exercise

---
+++ order_by_exercises.sql
@@ -0,0 +1,25 @@
+use employees;
+
+select * from employees where first_name in ('Irene','Vidya', 'Maya');
+
+select * from employees where last_name like 'E%';
+
+select * from employees where last_name like '%q%';
+
+select * from employees where first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya';
+
+select * from employees where (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya') and gender = 'M';
+
+select * from employees where last_name like 'E%' or last_name like '%E';
+
+select * from employees where last_name like 'E%' and last_name like '%E';
+
+select * from employees where last_name like'%q%' and last_name not like '%q%';
+--modify--
+select * from employees where first_name in ('Irene','Vidya', 'Maya') order by first_name;
+
+select * from employees where first_name in ('Irene','Vidya', 'Maya') order by first_name, last_name;
+
+select * from employees where first_name in ('Irene','Vidya', 'Maya') order by last_name, first_name;
+
+select * from employees where last_name like 'E%' or last_name like '%E';
